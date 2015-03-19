79c086010728b33ccb15677b0e71c6b2e90a0eba
Kris Jenkins <krisajenkins@gmail.com>
2015-03-19 20:55:30 +0000

    WIN!

--- Main.hs
+++ Main.hs
@@ -1,13 +1,65 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import           Control.Applicative ((<$>))
+import           Data.Char
+import           Data.List
+import           Data.Maybe
+import qualified Data.MultiSet       as MS
+import           Data.Ord
 import           Data.Text           (Text)
 import qualified Data.Text           as T
 import qualified Data.Text.IO        as TIO
+import qualified Data.Tree           as Tree
+import           System.Environment
 
-loadDictionary :: FilePath -> IO [Text]
-loadDictionary file = T.lines <$> TIO.readFile file
+type Dictionary = [Text]
+type Remainder = MS.MultiSet Char
+type Word = Text
+type Anagram = ([Word], Remainder)
+
+dullWord :: Word -> Bool
+dullWord "a" = False
+dullWord "i" = False
+dullWord s = T.length s <= 1
+
+loadDictionary :: FilePath -> IO Dictionary
+loadDictionary file =
+  (sortBy (flip (comparing T.length)) .
+   filter (not . dullWord) .
+   T.lines) <$>
+  TIO.readFile file
+
+extractWord :: Remainder -> Word -> Maybe Remainder
+extractWord remainderSet word =
+  if MS.isSubsetOf wordSet remainderSet
+     then Just (MS.difference remainderSet wordSet)
+     else Nothing
+  where wordSet = MS.fromList (T.unpack $ T.toLower word)
+
+unfoldAnagram :: Dictionary -> Anagram -> (Anagram, [Anagram])
+unfoldAnagram dictionary anagramSoFar@(wordsSoFar,remainderSoFar) =
+  (anagramSoFar,bar)
+  where bar = mapMaybe g dictionary
+        g newWord =
+          case extractWord remainderSoFar newWord of
+            Nothing -> Nothing
+            Just newRemainder ->
+              Just (newWord : wordsSoFar,newRemainder)
+
+
+anagrams :: Dictionary -> Word -> [[Word]]
+anagrams dictionary word =
+  map fst $
+  filter (\(_,remainder) -> MS.null remainder) $
+  Tree.flatten $
+  Tree.unfoldTree (unfoldAnagram dictionary)
+                  ([],MS.fromList (T.unpack $ T.toLower word))
 
 main :: IO ()
-main = do ws <- loadDictionary "/usr/share/dict/words"
-          print $ take 5 ws
+main =
+  do [phrase] <- getArgs
+     ws <- loadDictionary "/usr/share/dict/words"
+     mapM_ (putStrLn . T.unpack . T.unwords) $
+       anagrams ws (T.pack $ filter (not . isSpace) phrase)
